18b15008cc9f19f7481ac42d98c21ecec5158f7b
fatimazaarate <fatima.zaarate@gmail.com>
2024-06-04 16:29:51 +0100

    task_4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,10 @@
 /*eslint-disable*/
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+  .filter(student => student.location === city)
+  .map(student => {
+    const newGrade = newGrades.find(grade => grade.studentId === student.id);
+    const grade = newGrade ? newGrade.grade : 'N/A';
+    return { ...student, grade };
+  });
+}
