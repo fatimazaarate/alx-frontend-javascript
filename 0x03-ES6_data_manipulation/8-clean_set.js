eccbb2cec59ca79983e9a8ef3c4fa8a1516f1d73
fatimazaarate <fatima.zaarate@gmail.com>
2024-06-06 13:43:43 +0100

    mandatory

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 /*eslint-disable*/
 export default function cleanSet(set, startString) {
-  if (startString === '' ||
-    typeof startString !== 'string' ||
-    startString.length === 0
-  ) return '';
-
-  return [...set]
-  .filter(str => str.startsWith(startString))
-  .map(str => str.slice(startString.length))
-  .join('-');
+  if (startString === '' || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+  const stringUni = [];
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      // WE'RE SLICING value str from the index equivalent to length of startstring
+      stringUni.push(value.slice(startString.length));
+    }
+  }
+  return stringUni.join('-');
 }
